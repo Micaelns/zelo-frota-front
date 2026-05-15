3bab2ba4407c6ff08396bc3726e58fb81e3afaec
Micael Souza <micael.ns@hotmail.com>
2026-05-14 23:09:31 -0300

    comit inicial

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+//import { useState } from "react";
+import "./App.css";
+
+function App() {
+  //const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold">Hello Tailwind ss</h1>
+        Hello T
+      </div>
+    </>
+  );
+}
+
+export default App;
