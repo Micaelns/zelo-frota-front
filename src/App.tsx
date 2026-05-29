0a64bb04065039b375b3c2b24fd81490c684d0f2
Micael Souza <micael.ns@hotmail.com>
2026-05-29 12:47:17 -0300

    chore: permitir o uso do Toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { AppRoutes } from "./app/routes/AppRoutes";
+import { ToastProvider } from "./context/toast/ToastProvider";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <ToastProvider>
+      <AppRoutes />
+    </ToastProvider>
+  );
 }
 
 export default App;
--- src/app/layouts/DefaultLayout.tsx
+++ src/app/layouts/DefaultLayout.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "./Slidebar";
 import { Topbar } from "./Topbar";
+import { ToastContainer } from "../../components/feedback/ToastContainer";
 
 export function DefaultLayout() {
   const [collapsed, setCollapsed] = useState(true);
@@ -17,6 +18,7 @@ export function DefaultLayout() {
         />
 
         <main className="flex-1 p-6">
+          <ToastContainer />
           <Outlet />
         </main>
       </div>
